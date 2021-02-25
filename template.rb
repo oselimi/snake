d60123d3c62e65426770d6d7cef8b71aabf910be
oselimi <osman.94@live.com>
2021-02-25 18:20:38 +0100

    Add Tailwind

--- template.rb
+++ template.rb
@@ -1,77 +1,12 @@
-require "fileutils"
-require "shellwords"
-# Add this template directory to source_paths so that Thor actions like
-# copy_file and template resolve against our source files. If this file was
-# invoked remotely via HTTP, that means the files are not present locally.
-# In that case, use `git clone` to download them to a local temporary dir.
-def add_template_repository_to_source_path
-  if __FILE__ =~ %r{\Ahttps?://}
-    require "tmpdir"
-    source_paths.unshift(tempdir = Dir.mktmpdir("snake-"))
-    at_exit { FileUtils.remove_entry(tempdir) }
-    git clone: [
-      "--quiet",
-      "https://github.com/oselimi/snake.git",
-      tempdir
-    ].map(&:shellescape).join(" ")
-
-    if (branch = __FILE__[%r{snake/(.+)/template.rb}, 1])
-      Dir.chdir(tempdir) { git checkout: branch }
-    end
-  else
-    source_paths.unshift(File.dirname(__FILE__))
-  end
-end
-
-def rails_version
-  @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
-end
-
-def rails_5?
-  Gem::Requirement.new(">= 5.2.0", "< 6.0.0.beta1").satisfied_by? rails_version
-end
-
-def rails_6?
-  Gem::Requirement.new(">= 6.0.0.beta1", "< 7").satisfied_by? rails_version
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))]
 end
 
 def add_gems
-  gem 'administrate', github: "excid3/administrate", branch: 'jumpstart'
-  gem 'bootstrap', '~> 4.5'
-  gem 'devise', '~> 4.7', '>= 4.7.1'
-  gem 'devise-bootstrapped', github: 'excid3/devise-bootstrapped', branch: 'bootstrap4'
-  gem 'devise_masquerade', '~> 1.2'
-  gem 'font-awesome-sass', '~> 5.13'
-  gem 'friendly_id', '~> 5.3'
-  gem 'image_processing'
-  gem 'mini_magick', '~> 4.10', '>= 4.10.1'
-  gem 'name_of_person', '~> 1.1'
-  gem 'noticed', '~> 1.2'
-  gem 'omniauth-facebook', '~> 6.0'
-  gem 'omniauth-github', '~> 1.4'
-  gem 'omniauth-twitter', '~> 1.4'
-  gem 'pundit', '~> 2.1'
-  gem 'redis', '~> 4.2', '>= 4.2.2'
-  gem 'sidekiq', '~> 6.0', '>= 6.0.3'
-  gem 'sitemap_generator', '~> 6.1', '>= 6.1.2'
-  gem 'whenever', require: false
-
-  if rails_5?
-    gsub_file "Gemfile", /gem 'sqlite3'/, "gem 'sqlite3', '~> 1.3.0'"
-    gem 'webpacker', '~> 5.1', '>= 5.1.1'
-  end
-end
-
-def set_application_name
-  # Add Application Name to Config
-  if rails_5?
-    environment "config.application_name = Rails.application.class.parent_name"
-  else
-    environment "config.application_name = Rails.application.class.module_parent_name"
-  end
-
-  # Announce the user where they can change the application name in the future.
-  puts "You can change application name inside: ./config/application.rb"
+  gem 'devise', '~> 4.7', '>= 4.7.3'
+  gem 'friendly_id', '~> 5.4', '>= 5.4.1'
+  gem 'sidekiq', '~> 6.1', '>= 6.1.2'
+  gem 'name_of_person', '~> 1.1', '>= 1.1.1'
 end
 
 def add_users
@@ -81,83 +16,41 @@ def add_users
   # Configure Devise
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
               env: 'development'
+
   route "root to: 'home#index'"
 
-  # Devise notices are installed via Bootstrap
-  generate "devise:views:bootstrapped"
-
   # Create Devise User
-  generate :devise, "User",
-           "first_name",
-           "last_name",
-           "admin:boolean"
+  generate :devise, "User", "first_name", "last_name", "admin:boolean"
 
-  # Set admin default to false
+  # set admin boolean to false by default
   in_root do
     migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
     gsub_file migration, /:admin/, ":admin, default: false"
   end
 
-  if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
-    gsub_file "config/initializers/devise.rb",
-      /  # config.secret_key = .+/,
-      "  config.secret_key = Rails.application.credentials.secret_key_base"
-  end
-
-  # Add Devise masqueradable to users
-  inject_into_file("app/models/user.rb", "omniauthable, :masqueradable, :", after: "devise :")
-end
-
-def add_authorization
-  generate 'pundit:install'
-end
-
-def add_webpack
-  # Rails 6+ comes with webpacker by default, so we can skip this step
-  return if rails_6?
-
-  # Our application layout already includes the javascript_pack_tag,
-  # so we don't need to inject it
-  rails_command 'webpacker:install'
-end
-
-def add_javascript
-  run "yarn add expose-loader jquery popper.js bootstrap data-confirm-modal local-time"
-
-  if rails_5?
-    run "yarn add turbolinks @rails/actioncable@pre @rails/actiontext@pre @rails/activestorage@pre @rails/ujs@pre"
-  end
-
-  content = <<-JS
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Rails: '@rails/ujs'
-}))
-  JS
-
-  insert_into_file 'config/webpack/environment.js', content + "\n", before: "module.exports = environment"
-
-  append_to_file "app/assets/config/manifest.js", <<-RUBY
-//= link administrate/application.css
-//= link administrate/application.js
-  RUBY
+  # name_of_person gem
+  append_to_file("app/models/user.rb", "\nhas_person_name\n", after: "class User < ApplicationRecord")
 end
 
 def copy_templates
-  remove_file "app/assets/stylesheets/application.css"
-
-  copy_file "Procfile"
-  copy_file "Procfile.dev"
-  copy_file ".foreman"
-
   directory "app", force: true
-  directory "config", force: true
-  directory "lib", force: true
+end
 
-  route "get '/terms', to: 'home#terms'"
-  route "get '/privacy', to: 'home#privacy'"
+def add_tailwind
+  # Until PostCSS 8 ships with Webpacker/Rails we need to run this compatability version
+  # See: https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
+  run "yarn add tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9"
+  run "mkdir -p app/javascript/stylesheets"
+
+  append_to_file("app/javascript/packs/application.js", 'import "stylesheets/application"')
+  inject_into_file("./postcss.config.js", "\n    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),", after: "plugins: [")
+
+  run "mkdir -p app/javascript/stylesheets/components"
+end
+
+# Remove Application CSS
+def remove_app_css
+  remove_file "app/assets/stylesheets/application.css"
 end
 
 def add_sidekiq
@@ -175,138 +68,42 @@ def add_sidekiq
   insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
 end
 
-
-def add_administrate
-  generate "administrate:install"
-
-  append_to_file "app/assets/config/manifest.js" do
-    "//= link administrate/application.css\n//= link administrate/application.js"
-  end
-
-  gsub_file "app/dashboards/announcement_dashboard.rb",
-    /announcement_type: Field::String/,
-    "announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
-
-  gsub_file "app/dashboards/user_dashboard.rb",
-    /email: Field::String/,
-    "email: Field::String,\n    password: Field::String.with_options(searchable: false)"
-
-  gsub_file "app/dashboards/user_dashboard.rb",
-    /FORM_ATTRIBUTES = \[/,
-    "FORM_ATTRIBUTES = [\n    :password,"
-
-  gsub_file "app/controllers/admin/application_controller.rb",
-    /# TODO Add authentication logic here\./,
-    "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
-
-  environment do <<-RUBY
-    # Expose our application's helpers to Administrate
-    config.to_prepare do
-      Administrate::ApplicationController.helper #{@app_name.camelize}::Application.helpers
-    end
-  RUBY
-  end
-end
-
-def add_multiple_authentication
-    insert_into_file "config/routes.rb",
-    ', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }',
-    after: "  devise_for :users"
-
-    generate "model Service user:references provider uid access_token access_token_secret refresh_token expires_at:datetime auth:text"
-
-    template = """
-    env_creds = Rails.application.credentials[Rails.env.to_sym] || {}
-    %i{ facebook twitter github }.each do |provider|
-      if options = env_creds[provider]
-        config.omniauth provider, options[:app_id], options[:app_secret], options.fetch(:options, {})
-      end
-    end
-    """.strip
-
-    insert_into_file "config/initializers/devise.rb", "  " + template + "\n\n",
-          before: "  # ==> Warden configuration"
-end
-
-def add_whenever
-  run "wheneverize ."
+def add_foreman
+  copy_file "Procfile"
 end
 
 def add_friendly_id
   generate "friendly_id"
-
-  insert_into_file(
-    Dir["db/migrate/**/*friendly_id_slugs.rb"].first,
-    "[5.2]",
-    after: "ActiveRecord::Migration"
-  )
 end
-
-def stop_spring
-  run "spring stop"
-end
-
-def add_sitemap
-  rails_command "sitemap:install"
-end
-
-def add_tailwind
-  # Until PostCSS 8 ships with Webpacker/Rails we need to run this compatability version
-  # See: https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
-  run "yarn add tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9"
-  run "mkdir -p app/javascript/stylesheets"
- 
-  append_to_file("app/javascript/packs/application.js", 'import "stylesheets/application"')
-  inject_into_file("./postcss.config.js", "\n    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),", after: "plugins: [")
- 
-  run "mkdir -p app/javascript/stylesheets/components"
- end
 
 # Main setup
-add_template_repository_to_source_path
+source_paths
 
 add_gems
 
 after_bundle do
-  set_application_name
-  stop_spring
   add_users
-  add_authorization
-  add_webpack
-  add_javascript
-  add_multiple_authentication
+  remove_app_css
   add_sidekiq
+  add_foreman
+  copy_templates
+  add_tailwind
   add_friendly_id
 
-  copy_templates
-  add_whenever
-  add_sitemap
-  add_tailwind
-  rails_command "active_storage:install"
+  # Migrate
+  rails_command "db:create"
+  rails_command "db:migrate"
 
-  # Commit everything to git
-  unless ENV["SKIP_GIT"]
-    git :init
-    git add: "."
-    # git commit will fail if user.email is not configured
-    begin
-      git commit: %( -m 'Initial commit' )
-    rescue StandardError => e
-      puts e.message
-    end
-  end
+  git :init
+  git add: "."
+  git commit: %Q{ -m "Initial commit" }
 
   say
-  say "Snake app successfully created!", :blue
+  say "Kickoff app successfully created! 👍", :green
   say
-  say "To get started with your new app:", :green
-  say "  cd #{app_name}"
+  say "Switch to your app by running:"
+  say "$ cd #{app_name}", :yellow
   say
-  say "  # Update config/database.yml with your database credentials"
-  say
-  say "  rails db:create && rails db:migrate"
-  say "  rails g administrate:install # Generate admin dashboards"
-  say "  gem install foreman"
-  say "  foreman start # Run Rails, sidekiq, and webpack-dev-server"
+  say "Then run:"
+  say "$ rails server", :green
 end
-
